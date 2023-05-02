d52ec2bc755c78304c71f0386f9d30530f0a4a4b
Helarman <helarman@bk.ru>
2023-05-02 09:50:09 +0300

    new review page

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,6 @@
 import bootstrap from "../src/css/bootstrap.css"
 import animate from "../src/css/animate.css"
-import styles from "../src/css/style.scss"
+
 import { QueryClientProvider, QueryClient } from "react-query";
 
 const queryClient = new QueryClient()
@@ -9,7 +9,7 @@ const queryClient = new QueryClient()
 const App = ({ Component, pageProps }) => {
 
   return (
-    <div className={styles.burgerBodySettings} >
+    <div >
       <QueryClientProvider client={queryClient}>
         <Component  {...pageProps} />
       </QueryClientProvider>
